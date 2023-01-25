0539940e39df468d27a08e42895d81275e1af35e
Robert Moraes <robertmoraes13r@gmail.com>
2023-01-25 14:17:52 -0300

    Criação da estrutura e estilização da home

--- index.php
+++ index.php
@@ -2,9 +2,50 @@
 include_once("templates/header.php");
 ?>
 
-<h1>Testando Agenda</h1>
+<div class="container">
+    <?php if (isset($printMsg) && $printMsg != "") : ?>
+        <p id="message"><?= $printMsg ?></p>
+    <?php endif; ?>
 
-<i class="fas fa-eye"></i>
+    <h1 id="main-title">Minha Agenda</h1>
+
+    <?php if (count($contacts) > 0) : ?>
+        <table class="table" id="contacts-table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Telefone</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($contacts as $contact) : ?>
+                    <tr>
+                        <td scope="row"><strong><?= $contact["id"]; ?></strong></td>
+                        <td scope="row"><?= $contact["name"]; ?></td>
+                        <td scope="row"><?= $contact["phone"]; ?></td>
+                        <td class="actions">
+                            <a href="#">
+                                <i class="fas fa-eye check-icon"></i>
+                            </a>
+                            <a href="#">
+                                <i class="far fa-edit edit-icon"></i>
+                            </a>
+                            <button type="submit" class="delete-btn">
+                                <i class="fas fa-times delete-icon"></i>
+                            </button>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php else : ?>
+        <p id="empty-list-text">
+            Ainda não há contatos em sua agenda, <a href="<?= $BASE_URL; ?>create.php">clique aqui para adicionar</a>
+        </p>
+    <?php endif; ?>
+</div>
 
 <?php
 include_once("templates/footer.php");
