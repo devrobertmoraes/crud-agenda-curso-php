d9befb4db729954f24e8cf0a3ddf4b8e0568f2b4
Robert Moraes <robertmoraes13r@gmail.com>
2023-01-25 16:29:19 -0300

    Implementa a funcionalidade de atualizar

--- config/process.php
+++ config/process.php
@@ -30,6 +30,31 @@ if (!empty($data)) {
             $error = $e->getMessage();
             echo "Erro: $error";
         }
+    } else if ($data["type"] === "edit") {
+
+        $name = $data["name"];
+        $phone = $data["phone"];
+        $observations = $data["observations"];
+        $id = $data["id"];
+
+        $query = "UPDATE contacts 
+                  SET name = :name, phone = :phone, observations = :observations 
+                  WHERE id = :id";
+
+        $stmt = $connection->prepare($query);
+
+        $stmt->bindParam(":name", $name);
+        $stmt->bindParam(":phone", $phone);
+        $stmt->bindParam(":observations", $observations);
+        $stmt->bindParam(":id", $id);
+
+        try {
+            $stmt->execute();
+            $_SESSION["message"] = "Contato atualizado com sucesso";
+        } catch (PDOException $e) {
+            $error = $e->getMessage();
+            echo "Erro: $error";
+        }
     }
 
     // Redirecionar para a Home
--- edit.php
+++ edit.php
@@ -32,5 +32,5 @@ include_once("templates/header.php");
 </div>
 
 <?php
-include_once("templates/footer.php");
+include_once("templates/footer.php"); 
 ?>
